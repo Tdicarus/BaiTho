1012262592b52f9f795f2a59218d842a08cf0f99
Tdicarus <ngt679@gmail.com>
2022-05-24 20:01:55 +0700

    Initial commit

---
+++ app/src/main/java/ngothuctridat/sv61133478/baitho/MainActivity.java
@@ -0,0 +1,13 @@
+package ngothuctridat.sv61133478.baitho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
