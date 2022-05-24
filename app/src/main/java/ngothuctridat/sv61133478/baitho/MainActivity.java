567e83e3bca1a2345ee16230460f0a2c1e96ca5c
Tdicarus <ngt679@gmail.com>
2022-05-24 20:23:05 +0700

    Bai2

--- app/src/main/java/ngothuctridat/sv61133478/baitho/MainActivity.java
+++ app/src/main/java/ngothuctridat/sv61133478/baitho/MainActivity.java
@@ -2,12 +2,37 @@ package ngothuctridat.sv61133478.baitho;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private AdapterView adapterView;
+    private ArrayAdapter arrayAdapter;
+    private ListView listView;
+    private Button them;
+    private Button xoa;
+    private EditText nhapten;
+    private ArrayList arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        them = (Button) findViewById(R.id.them);
+        xoa = (Button) findViewById(R.id.xoa);
+        listView = (ListView) findViewById(R.id.listView);
+        
+
+
     }
+
+
+
 }
